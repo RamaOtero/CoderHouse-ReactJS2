cabace37dbaf8f0bfcd384db38f1b1b071655892
Rama Otero <ramirootero98@gmail.com>
2022-07-05 12:12:06 -0300

    item

---
+++ src/components/ItemListContainer/ItemList/Item/Item.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../../../../App.css';
+
+const Item = ({ item }) => {
+
+    const { name, price, image } = item;
+
+    return (
+        <div className="item">
+            <img src={image} alt={name} className="itemImg" />
+            <h5> {name} </h5>
+            <p> $ {price} </p>
+        </div>
+    );
+};
+
+export default Item;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import "../../App.css";
+import ItemList from './ItemList/ItemList.js';
+import { useState } from "react";
+import { productList } from '../Products/products'
 
 const ItemListContainer = ({greetings}) => {
+  const [items, setItems] = useState([]);
+
+    const data = new Promise((resolve, reject) => {
+        resolve(productList)
+    });
+
+    data.then((data) => {
+        setItems(data)
+    });
+
+    data.catch((error) => {
+        console.error(error)
+    });
+
   return (
     <div className='ItemListContainer__Span'>
         <span>
             New Arrival | {greetings}
         </span>
+            <ItemList items={items} />
     </div>
   )
 }
